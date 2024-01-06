2d4b2cd39bc6478a15070cc1ca123ec29edf5405
Dreeeems <114916033+Dreeeems@users.noreply.github.com>
2024-01-06 16:12:25 +0100

    Add request and update controller

--- app/Http/Controllers/api/postController.php
+++ app/Http/Controllers/api/postController.php
@@ -3,20 +3,23 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CreatePostRequest;
 use Illuminate\Http\Request;
+use App\Models\Posts;
 
 class postController extends Controller
 {
-     public function index(){
+    public function index()
+    {
         return 'Liste';
-     }
+    }
 
-     public function store(){
+    public function store(CreatePostRequest $request)
+    {
 
         $post = new Posts();
-
         $post->title = "titre exemple";
         $post->description = "Desc exemple";
         $post->save();
-     }
+    }
 }
